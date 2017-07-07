56a686d35b64248bf785f8997a07728c4ae49af7
MinamiKotoriCute <MinamiKotoriCute@gmail.com>
2017-07-07 14:41:41 +0800

    加入readme.txt
    readme.txt一個空的文字檔，測試中

--- main.cpp
+++ main.cpp
@@ -5,16 +5,28 @@
 
 #include "agent.h"
 
+#include "maptreewidget.h"
+#include "easywebcontrol.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    /// defaulf, no change
     MainWindow w;
     w.show();
 
+    /// player game window
     //LoveLetterWidget lw;
     //lw.show();
 
-    Agent ag;
+    /// learning agent
+    //Agent ag;
+
+    /// show agent result
+    MapTreeWidget m;
+    m.setData(loadJson());
+    m.show();
 
     return a.exec();
 }
